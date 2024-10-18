02425590a91867c8929cffb8c517b7fe08298cab
reem <reemshaye1@gmail.com>
2024-10-18 18:52:17 +0300

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Reem on 15/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournalTests/JournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalTests.swift
+//  JournalTests
+//
+//  Created by Reem on 15/04/1446 AH.
+//
+
+import Testing
+@testable import Journal
+
+struct JournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
